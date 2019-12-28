d507ebaa80301af14aea9b1bdcc78063c3e96627
ShaddamA <47707535+ShaddamA@users.noreply.github.com>
2019-12-28 16:29:53 +0700

    Add files via upload

--- soal4/src/index.go
+++ soal4/src/index.go
@@ -11,17 +11,14 @@ func main()  {
 
 func print(a string){
 	angka := [10]string{"0","1","2","3","4","5","6","7","8","9"}
-	var arrayUrut = [...]string{}
 
 	b := strings.Split(a,"")
 	count := len(b)
 	for i:= 0; i < count; i++{
 		for j := 0; j < 10; j++ {
 			if (b[i] == angka[j]) {
-				fmt.Println(b[i])
+				fmt.Print(b[i])
 			}	
-		}
-		
+		}	
 	}
-	fmt.Println(arrayUrut) 
 }
