3fb5b151f6dfe1ffe50bde3b6eaa65b939b92f4f
ShaddamA <47707535+ShaddamA@users.noreply.github.com>
2019-12-28 15:08:14 +0700

    Add files via upload

---
+++ soal4/src/index.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"strings"
+	"fmt"
+)
+
+func main()  {
+	print("48172a94")
+}
+
+func print(a string){
+	angka := [10]string{"0","1","2","3","4","5","6","7","8","9"}
+	var arrayUrut = [...]string{}
+
+	b := strings.Split(a,"")
+	count := len(b)
+	for i:= 0; i < count; i++{
+		for j := 0; j < 10; j++ {
+			if (b[i] == angka[j]) {
+				fmt.Println(b[i])
+			}	
+		}
+		
+	}
+	fmt.Println(arrayUrut) 
+}
